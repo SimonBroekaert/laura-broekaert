3928533b058c8ac019d33816dc9cbf4d7a9bca0f
Simon Broekaert <simon_broekaert@hotmail.com>
2023-01-23 23:39:41 +0100

    Add trusty nova macros

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,9 +2,15 @@
 
 namespace App\Providers;
 
+use App\Nova\Dashboards\Main;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
+use KABBOUCHI\LogsTool\LogsTool;
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
+use Visanduma\NovaBackNavigation\NovaBackNavigation;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
@@ -16,6 +22,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        Nova::withBreadcrumbs();
     }
 
     /**
@@ -26,9 +34,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function routes()
     {
         Nova::routes()
-                ->withAuthenticationRoutes()
-                ->withPasswordResetRoutes()
-                ->register();
+            ->withAuthenticationRoutes()
+            ->withPasswordResetRoutes()
+            ->register();
     }
 
     /**
@@ -41,9 +49,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return true;
         });
     }
 
@@ -55,7 +61,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function dashboards()
     {
         return [
-            new \App\Nova\Dashboards\Main,
+            new Main(),
         ];
     }
 
@@ -66,7 +72,13 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     public function tools()
     {
-        return [];
+        return [
+            new NovaBackNavigation(),
+            (new LogsTool())
+                ->canSee(function ($request) {
+                    return $request->user()->is_developer;
+                }),
+        ];
     }
 
     /**
@@ -76,6 +88,81 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     public function register()
     {
-        //
+        Nova::sortResourcesBy(function ($resource) {
+            return $resource::$priority ?? 99999;
+        });
+
+        Field::macro('dateFormat', function () {
+            /** @var \Laravel\Nova\Fields\Field $this */
+            return $this->displayUsing(function ($value) {
+                return $value->isoFormat('dddd, LL');
+            });
+        });
+
+        Field::macro('datetimeFormat', function () {
+            /** @var \Laravel\Nova\Fields\Field $this */
+            return $this->displayUsing(function ($value) {
+                $date = $value->isoFormat('dddd, LL');
+                $time = $value->isoFormat('LT');
+                $glue = __('nova.datetime.at');
+
+                return "{$date} {$glue} {$time}";
+            });
+        });
+
+        Field::macro('hideFromRelationshipIndex', function () {
+            if (NovaRequest::createFrom(request())->viaRelationship()) {
+                /** @var \Laravel\Nova\Fields\Field $this */
+                return $this->hideFromIndex();
+            }
+
+            return $this;
+        });
+
+        Field::macro('autofillFrom', function ($field) {
+            if (! NovaRequest::createFrom(request())->viaRelationship()) {
+                return $this;
+            }
+
+            if (! NovaRequest::createFrom(request())->isCreateOrAttachRequest()) {
+                return $this;
+            }
+
+            $fieldStack = explode('.', $field);
+            $relation = array_shift($fieldStack);
+            $subRelations = array_slice($fieldStack, 0, -1);
+            $attribute = end($fieldStack);
+
+            $viaResource = NovaRequest::createFrom(request())->viaResource();
+
+            if (Str::camel(basename($viaResource::$model)) !== $relation) {
+                return $this;
+            }
+
+            $model = NovaRequest::createFrom(request())->findParentModel();
+
+            if (is_null($model)) {
+                return $this;
+            }
+
+            foreach ($subRelations as $subRelation) {
+                $model = $model->{$subRelation} ?? null;
+
+                if (is_null($model)) {
+                    return $this;
+                }
+            }
+
+            $value = $model->{$attribute} ?? null;
+
+            if (is_null($value)) {
+                return $this;
+            }
+
+            /** @var \Laravel\Nova\Fields\Field $this */
+            return $this->withMeta([
+                'value' => $value,
+            ]);
+        });
     }
 }

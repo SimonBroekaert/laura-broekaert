ed02544669604b14e507fbc4bef7baff12cd08c8
Simon Broekaert <simon_broekaert@hotmail.com>
2023-03-03 00:55:09 +0100

    Fixed gdpr tab index

--- resources/views/components/form/gdpr.blade.php
+++ resources/views/components/form/gdpr.blade.php
@@ -7,11 +7,13 @@
 @endphp
 
 <label {{ $attributes }}>
-	<input class="absolute invisible peer/gdpr -z-1" type="checkbox" id="{{ $key }}"
-		wire:model="{{ $key }}" />
-	<div
-		class="p-10 transition-all border rounded-sm cursor-pointer rfs:text-lg border-gray bg-white peer-checked/gdpr:border-primary text-primary [--child-opacity:0] peer-checked/gdpr:[--child-opacity:1]">
-		<x-svg.check class="opacity-[var(--child-opacity,0)]" />
+	<div class="relative">
+		<input class="absolute opacity-0 peer/gdpr -z-1" type="checkbox" id="{{ $key }}"
+			wire:model="{{ $key }}" />
+		<div
+			class="p-10 transition-colors border rounded-sm cursor-pointer rfs:text-lg border-gray bg-white peer-checked/gdpr:border-primary text-primary [--child-opacity:0] peer-checked/gdpr:[--child-opacity:1] peer-focus/gdpr:border-primary peer-focus/gdpr:border-2">
+			<x-svg.check class="opacity-[var(--child-opacity,0)]" />
+		</div>
 	</div>
 	<p class="cursor-pointer rfs:text-base">
 		@if ($privacyPage)
